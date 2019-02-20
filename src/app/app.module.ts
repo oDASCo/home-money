ae033e4fd4fa35b744f006dd8f9d858df55c2976
Daria Lazovskaya <32599716+odasco@users.noreply.github.com>
2019-02-21 01:32:44 +0300

    87

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {FormsModule} from "@angular/forms";
-import {AuthModule} from "./auth/auth.module";
-import {HttpClientModule} from "@angular/common/http";
-import {AppRoutingModule} from "./app-routing.module";
+
+import {AuthModule} from './auth/auth.module';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
     declarations: [
